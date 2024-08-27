5e385c8b706ee00e5fda66cdc82c2f2474db2167
Pankaj Patel <panky_68@hotmail.com>
2024-08-27 22:08:19 +0100

    added form submission with form validation

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -4,32 +4,33 @@ import TodoItem from "./ToDoItem";
 import Modal from "./Modal";
 
 const ToDoList = ({ showModal, onHideModal }) => {
-  const [enteredTodo, setEnteredTodo] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  function setTodoHandler(event) {
-    setEnteredTodo(event.target.value);
-  }
-
-  function setDateHandler(event) {
-    setEnteredDate(event.target.value);
+  function addTodoHandler(enteredTodo) {
+    setTodos((existingTodos) => [enteredTodo, ...existingTodos]);
   }
 
   return (
     <>
       {showModal && (
         <Modal onCloseModal={onHideModal}>
-          <NewTodo
-            setEnteredTodo={setTodoHandler}
-            setEnteredDate={setDateHandler}
-            onCancel={onHideModal}
-          />
+          <NewTodo onSubmit={addTodoHandler} onCancel={onHideModal} />
         </Modal>
       )}
-      <ul className="grid gap-5 grid-cols-[repeat(3,30%)] justify-center">
-        <TodoItem date={enteredDate} todo={enteredTodo} />
-        <TodoItem date="22/2/2022" todo="Learn React" />
-      </ul>
+      {todos.length === 0 && (
+        <div className="block">
+          <p className="text-center text-3xl font-bold">Currently No todos</p>
+        </div>
+      )}
+
+      {todos.length > 0 && (
+        <ul className="grid gap-5 grid-cols-[repeat(3,30%)] justify-center">
+          {todos.length > 0 &&
+            todos.map((todo) => (
+              <TodoItem key={todo.id} date={todo.date} todo={todo.todo} />
+            ))}
+        </ul>
+      )}
     </>
   );
 };
