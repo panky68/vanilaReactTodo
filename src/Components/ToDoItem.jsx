f2782a7234e477dbca9608640d7fc382bf4b3237
Pankaj Patel <panky_68@hotmail.com>
2024-08-28 13:26:32 +0100

    Add delete todos (#2)
    * implemented delete todos
    
    * refactored validation code for new Todos

--- src/Components/ToDoItem.jsx
+++ src/Components/ToDoItem.jsx
@@ -1,7 +1,14 @@
-const TodoItem = ({ date, todo }) => {
+import { TiDeleteOutline } from "react-icons/ti";
+
+const TodoItem = ({ id, date, todo, onRemoveTodo }) => {
   return (
-    <li className="bg-yellow-300 text-black rounded shadow-[0_0_10px_rgba(0,0,0,0.3)] pl-5">
-      <h1 className="text-3xl font-bold pb-5 pt-5">ToDo</h1>
+    <li className="bg-yellow-300 text-black rounded shadow-[0_0_10px_rgba(0,0,0,0.3)] pl-5 pr-5">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold pb-5 pt-5">ToDo</h1>
+        <button onClick={() => onRemoveTodo(id)}>
+          <TiDeleteOutline />
+        </button>
+      </div>
       <p>{todo}</p>
       <p className="pb-5">Date: {date}</p>
     </li>
